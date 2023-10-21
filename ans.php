1866565e20b8120153a51b5ed8fbdffc6519026f
AlexxBelous <connectionwithme1@gmail.com>
2023-10-21 17:23:55 +0300

    start

---
+++ ans.php
@@ -0,0 +1,5 @@
+<!-- Start -->
+<!-- Список всех task: -git branch -a -->
+<!-- Переключение -git checkout task__1, task__2, task__3 -->
+<!-- Очищение документа -git checkout . -->
+<!--Path in the browser to display the response http://localhost/quiz/ans.php-->
