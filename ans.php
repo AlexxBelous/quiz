156df955d0227bcc282bae3b73afd68c5a5cf913
AlexxBelous <connectionwithme1@gmail.com>
2023-10-27 06:39:32 +0300

    fix

--- ans.php
+++ ans.php
@@ -11,3 +11,5 @@ $creditCardNumber = "1234 5578 9132 3343";
 echo "Original number card: $creditCardNumber<br>";
 
 
+
+
--- que.php
+++ que.php
@@ -1,8 +1,3 @@
-
-
-
-
-
 <!--
 
 Напишите код класса, который создает объект для обработки номера банковской карты, проверяет его валидность и скрывает большую часть номера, оставляя только последние 4 цифры. Код должен включать в себя код класса CreditCard, который выполняет указанные операции.
@@ -10,66 +5,39 @@
 -->
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!--******************************************-->
 
 <?php
 
-class CreditCard {
+class CreditCard
+{
     private $_number;
+    private $_cleanedNumber;
 
-    public function __construct($creditCardNumber) {
+    public function __construct($creditCardNumber)
+    {
         $this->_number = $creditCardNumber;
+        $this->_cleanedNumber = str_replace(' ', '', $creditCardNumber);
     }
 
-    public function isValid() {
-        $cleanedNumber = str_replace(' ', '', $this->_number);
-        return strlen($cleanedNumber) === 16;
+    public function isValid()
+    {
+        return strlen($this->_cleanedNumber) === 16;
     }
 
-    public function mask() {
+    public function mask()
+    {
         if (!$this->isValid()) {
-            return "<span style='color: red;'>Invalid card number.</span>";
+            return "Invalid card number.";
         }
 
-        $cleanedNumber = str_replace(' ', '', $this->_number);
-        $lastFourDigits = substr($cleanedNumber, -4);
+        $lastFourDigits = substr($this->_cleanedNumber, -4);
         $maskedPart = implode(' ', str_split(str_repeat('*', 12), 4));
 
         return $maskedPart . ' ' . $lastFourDigits;
     }
 }
+
 
 $creditCardNumber = "1234 5578 9012 3343";
 $creditCard = new CreditCard($creditCardNumber);
