f42c01c28adc2b687f72ff9829a7421fba70f755
AlexxBelous <connectionwithme1@gmail.com>
2023-10-28 18:08:06 +0300

    add task 6

--- que.php
+++ que.php
@@ -5,8 +5,9 @@
 
 <!--
 
-Напишите код, который будет объединять имен студентов и их возрастов из
-ассоциативных массивов в строку с использованием ", " после каждого имени и значения " - " между значениям возраста студентов.
+Необходимо создать объектно-ориентированный класс для обработки массива студентов, который позволяет получить имена студентов, разделенные запятыми, и возрасты, разделенные тире? Предложите код этого класса и его использование для данной задачи.
+
+Ответ на этот вопрос должен включать в себя объявление и использование класса StudentProcessor.
 
 -->
 
@@ -55,19 +56,34 @@ $students = [
     ["name" => "Charlie", "age" => 28, "city" => "Chicago"],
 ];
 
-// Concatenate student names with commas
-$names = [];
-foreach ($students as $student) {
-    $names[] = $student["name"];
-}
-$commaName = implode(', ', $names);
+class StudentProcessor {
+    private $students;
 
-// Concatenate student ages with dashes
-$ages = [];
-foreach ($students as $student) {
-    $ages[] = $student["age"];
+    public function __construct($students) {
+        $this->students = $students;
+    }
+
+    public function getNames() {
+        $names = array_map(function ($student) {
+            return $student["name"];
+        }, $this->students);
+
+        return implode(', ', $names);
+    }
+
+    public function getAges() {
+        $ages = array_map(function ($student) {
+            return $student["age"];
+        }, $this->students);
+
+        return implode(' - ', $ages);
+    }
 }
-$dashAges = implode(' - ', $ages);
+
+$studentProcessor = new StudentProcessor($students);
+
+$commaName = $studentProcessor->getNames();
+$dashAges = $studentProcessor->getAges();
 
 echo "Student Name: " . $commaName . "<br>";
 echo "Student Age: " . $dashAges;
