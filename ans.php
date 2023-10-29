dfde0254b06b2dc05a93b733bfb323a8685b87fd
AlexxBelous <connectionwithme1@gmail.com>
2023-10-29 06:55:31 +0200

    fix

--- ans.php
+++ ans.php
@@ -1,23 +1,7 @@
 <?php
 
 
-
 $names = ["Alice", "Bob", "Charlie"];
 
-// Исходный массив строк
-$names = ["Alice", "Bob", "Charlie"];
 
-// Определяем функцию, которую хотим применить
-function addGreeting($name) {
-    return "Hello, " . $name;
-}
-
-// Применяем функцию addGreeting() к каждой строке массива с помощью array_map
-$greetedNames = array_map('addGreeting', $names);
-
-// $greetedNames будет содержать ["Hello, Alice", "Hello, Bob", "Hello, Charlie"]
-
-
-
-echo implode("<", $greetedNames);
 
