540cf9f379a7923781256c66c52a05306212eebb
AlexxBelous <connectionwithme1@gmail.com>
2023-10-30 06:19:36 +0200

    add task 10

--- ans.php
+++ ans.php
@@ -1,5 +1,6 @@
 <?php
 
-$elements = ["apple", "123", "banana", "456", "cherry"];
+$text = "Under the shimmering moonlight, the night was filled with secrets. A gentle breeze rustled the leaves, whispering stories of distant lands. Stars adorned the velvety sky, like diamonds in the abyss. Nature's symphony played on, as creatures of the night began their nocturnal dances. An owl hooted in the distance, a mysterious serenade to the moon. The world slept, unaware of the magic that unfolded under the celestial canvas. Dreams took flight on the wings of imagination, weaving tales of wonder and enchantment. In this timeless night, possibilities were as boundless as the cosmos, and the universe held its breath, awaiting the next chapter of the unknown.";
+
 
 
--- que.php
+++ que.php
@@ -5,7 +5,7 @@
 
 <!--
 
-Напишите PHP-код, который принимает массив элементов и фильтрует его, оставляя только числовые значения. В результате выполнения кода должен быть получен новый массив, содержащий только цифры. "
+Напишите код, который позволяет разбить данный текст на слова и затем проверить, содержит ли этот текст слово 'nocturnal'. Если слово найдено, выведите сообщение 'The word 'nocturnal' is found in the text', в противном случае выведите 'The word 'nocturnal' is not found in the text'."
 
 -->
 
@@ -46,13 +46,17 @@
 
 <?php
 
-$elements = ["apple", "123", "banana", "456", "cherry"];
 
-$filteredNumber = array_filter($elements, function ($element) {
-    return is_numeric($element);
-});
+$text = "Under the shimmering moonlight, the night was filled with secrets. A gentle breeze rustled the leaves, whispering stories of distant lands. Stars adorned the velvety sky, like diamonds in the abyss. Nature's symphony played on, as creatures of the night began their nocturnal dances. An owl hooted in the distance, a mysterious serenade to the moon. The world slept, unaware of the magic that unfolded under the celestial canvas. Dreams took flight on the wings of imagination, weaving tales of wonder and enchantment. In this timeless night, possibilities were as boundless as the cosmos, and the universe held its breath, awaiting the next chapter of the unknown.";
 
-$result = implode(', ', $filteredNumber);
-echo $result;
 
+$words = str_word_count($text, 1);
+
+
+// Ищем слово "nocturnal" в массиве слов
+if (in_array("nocturnal", $words, true)) {
+    echo "Слово 'nocturnal' найдено в тексте.";
+} else {
+    echo "Слово 'nocturnal' не найдено в тексте.";
+}
 ?>
